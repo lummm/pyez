9984c8af48f2ad80ee0c89b856b80c3f920a02c7
Liam Tengelis <ltengelis@gmail.com>
2020-07-08 10:53:18 -0700

    v0.3.8

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import find_packages, setup
 
 
 setup(name='ez_arch_worker',
-      version='v0.3.7',
+      version='v0.3.8',
       description='Worker API for the ez_arch framework',
       author='Liam Tengelis',
       author_email='liam@tengelisconsulting.com',
       url='https://github.com/tengelisconsulting/ez_arch_worker',
       download_url=("https://github.com"
                     "/tengelisconsulting/ez_arch_worker"
-                    "/archive/v0.3.7.tar.gz"),
+                    "/archive/v0.3.8.tar.gz"),
       packages=find_packages(),
       package_data={
           '': ['*.yaml'],
