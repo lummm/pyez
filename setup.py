159cd8fce780dae2a82e61f316c36f7ce25d62e9
Liam Tengelis <ltengelis@gmail.com>
2020-07-09 19:31:04 -0700

    bump to v0.3.82

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import find_packages, setup
 
 
 setup(name='ez_arch_worker',
-      version='v0.3.81',
+      version='v0.3.82',
       description='Worker API for the ez_arch framework',
       author='Liam Tengelis',
       author_email='liam@tengelisconsulting.com',
       url='https://github.com/tengelisconsulting/ez_arch_worker',
       download_url=("https://github.com"
                     "/tengelisconsulting/ez_arch_worker"
-                    "/archive/v0.3.81.tar.gz"),
+                    "/archive/v0.3.82.tar.gz"),
       packages=find_packages(),
       package_data={
           '': ['*.yaml'],
