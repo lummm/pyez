828cb1a25ced1dc5561edc5b167e262f660277b8
Liam Tengelis <ltengelis@gmail.com>
2020-10-26 18:04:41 -0700

    clean up old lib

--- ez_arch_worker/test/framework.py
+++
@@ -1,9 +0,0 @@
-import zmq.asyncio
-
-
-async def init_input_output(port: int,) -> zmq.asyncio.Socket:
-    "returns sender"
-    c = zmq.asyncio.Context()
-    router = c.socket(zmq.ROUTER)
-    router.bind("tcp://*:{}".format(port))
-    return router
